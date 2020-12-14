66058598fd3dad26778c4cb4ff313cfcb332bc87
robinz62 <robinz672@gmail.com>
2020-12-13 19:19:14 -0500

    add filtering problems by tags

--- js/cf-picker.js
+++ js/cf-picker.js
@@ -1,23 +1,21 @@
 // Fetches a random problem with the given settings. Results from the codeforces
-// API are cached for problem selections (assuming no change in tags).
+// API are cached.
 //
 // The following data is cached in sessionStorage:
-// tags: a sorted list of tags. This is to detect if a new API call is needed.
 // problems: the list of problems.
 //
-// TODO(robinz62): implement tags
-// TODO(robinz62): implement filter by already solved, given handle
-// TODO(robinz62): error handling
+// TODO(robinz62): consider filtering by already solved, given handle.
 function getProblem() {
   if (typeof Storage === 'undefined' || !window.sessionStorage.getItem('problems')) {
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.responseType = 'json';
     xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        const problems = xmlHttp.response.result.problems;
-        getProblemFromList(problems);
-        window.sessionStorage.setItem('problems', JSON.stringify(problems));
+      if (xmlHttp.readyState != 4 || xmlHttp.status != 200) {
+        // Error
+        return;
       }
+      const problems = xmlHttp.response.result.problems;
+      getProblemFromList(problems);
     }
     xmlHttp.open('GET', 'https://codeforces.com/api/problemset.problems?lang=en', /*async=*/true);
     xmlHttp.send(null);
@@ -29,21 +27,38 @@ function getProblem() {
 // Given a list of candidate problems, selects a random one out of the list
 // within the rating range specified on the page. Updates the DOM with results.
 function getProblemFromList(problems) {
+  // Filter by rating.
   let ratingLower = parseInt(document.getElementById('rating-lower').value, 10);
   let ratingUpper = parseInt(document.getElementById('rating-upper').value, 10) || 5000;
   if (isNaN(ratingLower)) ratingLower = 0;
   if (isNaN(ratingUpper)) ratingUpper = 5000;
-  const ratingFiltered = problems.filter(problem => {
-    return problem.rating >= ratingLower && problem.rating <= ratingUpper;
+
+  // Filter by tags (if present).
+  let tags = document.getElementById('tags').value || '';
+  tags = tags.split(',');
+  const tagsSet = new Set();
+  for (const tag of tags) if (tag) tagsSet.add(tag);
+  const filtered = problems.filter(problem => {
+    if (!problem.rating) return false;
+    if (problem.rating < ratingLower || problem.rating > ratingUpper) return false;
+    let found = tagsSet.size == 0;
+    for (const tag of problem.tags) {
+      if (found) break;
+      if (tagsSet.has(tag)) found = true;
+    }
+    if (!found) return false;
+    return true;
   });
-  const N = ratingFiltered.length;
+
+  // Choose a result and update DOM.
+  const N = filtered.length;
   const resultLinkDiv = document.getElementById('result-link-div');
   const noResultDiv = document.getElementById('result-nolink-div');
   if (N == 0) {
     resultLinkDiv.style.display = 'none';
     noResultDiv.style.display = 'inline';
   } else {
-    const picked = ratingFiltered[Math.floor(Math.random() * N)];
+    const picked = filtered[Math.floor(Math.random() * N)];
     const resultLink = document.getElementById('result-link');
     resultLink.textContent = `${picked.contestId}${picked.index} - ${picked.name}`;
     resultLink.href = `https://codeforces.com/problemset/problem/${picked.contestId}/${picked.index}`;
