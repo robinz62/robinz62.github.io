0ea67796a10525468b63e7d56fd2e85f85b3f52f
Robin Zhang <robinz672@gmail.com>
2021-05-12 21:54:30 -0500

    improve collision polygons

--- js/physics.js
+++ js/physics.js
@@ -54,30 +54,63 @@ let runMatter = () => {
     // Objects
     const restitution = 0.8;
 
-    // Creates an object with a random initial position, rotation, angular velocity, and velocity.
-    let logo = (texture) => {
-        let [width, height] = getDimensions();
-        let x = Math.random() * width;
-        let y = Math.random() * height
+    // Sets the rotation, angular velocity, and velocity randomly.
+    function randomizeMotion(object) {
         let angle = Math.random() * 2 * Math.PI;
         let angularVelocity = Math.random() * 0.2 - 0.1;
         let velocity = {
-            x: Math.random() * 10 - 5,
-            y: Math.random() * 10 - 5
+            x: Math.random() * 16 - 8,
+            y: Math.random() * 16 - 8
         };
-        let object = Bodies.rectangle(x, y, 128, 128, { restitution, render: { sprite: { texture } } });
         Body.rotate(object, Math.random() * angle);
         Body.setAngularVelocity(object, angularVelocity)
         Body.setVelocity(object, velocity);
+    };
+
+    // Creates an object with a random initial position, rotation, angular velocity, and velocity.
+    let square = (texture) => {
+        let [width, height] = getDimensions();
+        let x = Math.random() * width;
+        let y = Math.random() * height
+        let object = Bodies.rectangle(x, y, 128, 128, { restitution, render: { sprite: { texture } } });
+        randomizeMotion(object);
         return object;
     }
 
-    let lle = logo('./img/lle.png');
-    let penn = logo('./img/penn.png');
-    let bentley = logo('./img/bentley.png');
-    let qualtrics = logo('./img/qualtrics.png');
-    let google = logo('./img/google.png');
-    let js = logo('./img/js.png');
+    let circle = (texture) => {
+        let [width, height] = getDimensions();
+        let x = Math.random() * width;
+        let y = Math.random() * height
+        let object = Bodies.circle(x, y, 64, { restitution, render: { sprite: { texture } } });
+        randomizeMotion(object);
+        Body.setInertia(object, object.inertia / 5);  // This value gives a good amount of spin.
+        return object;
+    }
+
+    let lle = square('./img/lle.png');
+    let bentley = square('./img/bentley.png');
+    let qualtrics = square('./img/qualtrics.png');
+    let google = circle('./img/google.png');
+    let js = circle('./img/js.png');
+
+    // This could be refined if desired.
+    let penn = Bodies.fromVertices(Math.random() * initWidth, Math.random() * initHeight, [
+        { x: -57, y: -65 },
+        { x: 56, y: -65 },
+        { x: 56, y: 4 },
+        { x: 48, y: 25 },
+        { x: 37, y: 40 },
+        { x: 22, y: 52 },
+        { x: 0, y: 63 },
+        { x: -22, y: 52 },
+        { x: -37, y: 40 },
+        { x: -48, y: 25 },
+        { x: -57, y: 4 },
+    ], {
+        restitution,
+        render: { sprite: { texture: './img/penn.png' } }
+    });
+    randomizeMotion(penn);
 
     // Dynamic borders is a little buggy. Only explicity resizes of the window
     // trigger updates e.g. toggling fullscreen or opening the developer console
